bbfc01db7dde2e3a056b498644411cbbbbf2901f
mohammed3200 <wwyuu799@gmail.com>
2024-10-10 14:50:30 +0200

    native Electron by React and TypeScript with Tailwindcss Jotai

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello from Electorn</span>
+    </div>
+  )
+}
+
+export default App
